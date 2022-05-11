4706445d6a66c375dd3ac7162a05018a6c104e1c
Mounika-Krish <61351729+Mounika-Krish@users.noreply.github.com>
2022-05-12 02:02:13 +0530

    Update bootstrap_variables.tf

--- modules/bootstrap/bootstrap_variables.tf
+++ modules/bootstrap/bootstrap_variables.tf
@@ -2,9 +2,9 @@
 # Define variables for Azure DevOps Seed Module
 ##
 
-variable "name_of_s3_bucket" ="bucket-name-batch6"
-variable "dynamo_db_table_name"= "aws-locks"
-variable "iam_user_name"="Administrator"
+variable "name_of_s3_bucket" {}
+variable "dynamo_db_table_name"{}
+variable "iam_user_name"{}
 variable "ado_iam_role_name"{}
 variable "aws_iam_policy_permits_name"{}
 variable "aws_iam_policy_assume_name"{}
